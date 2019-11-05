20fe35cab1643a2473fc5086265a7e4ffa453be4
EsraOzturk <esosh77@hotmail.com>
2019-11-04 21:36:00 -0500

    java

---
+++ src/Packageday19/JavaSetEx1.java
@@ -0,0 +1,30 @@
+package Packageday19;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class JavaSetEx1 {
+    public static void main(String[] args) {
+
+
+                System.out.println("ArrayList------------------------");
+                ArrayList<Integer> listNumbers = new ArrayList<>();
+                listNumbers.add(1);
+                listNumbers.add(1);
+                listNumbers.add(1);
+                System.out.println(listNumbers);
+
+                System.out.println("HashSet------------------------");
+                HashSet<Integer> setNumbers = new HashSet<>();
+                setNumbers.add(1);
+                setNumbers.add(1);
+                setNumbers.add(1);
+                System.out.println(setNumbers);
+            }
+        }
+
+
+
+
+
+
